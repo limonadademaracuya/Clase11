4f20984113b7bb0aa786f1d939296348121a6fa2
limonadademaracuya <126106463+limonadademaracuya@users.noreply.github.com>
2023-10-31 23:05:31 -0300

    Add files via upload
    Ejercicios imprimir figuras por consola con el for

---
+++ vEjercicios Repaso for/src/vejercicios/repaso/pkgfor/VEjerciciosRepasoFor.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vejercicios.repaso.pkgfor;
+
+/**
+ *
+ * @author snowf
+ */
+public class VEjerciciosRepasoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Ejercicio 1:
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            
+        }
+        
+        // Ejercicio 2:
+        
+        for (int i = 1; i <= 10; i+=2) {
+            System.out.println(i);
+        }
+        
+        // Ejercicio 3:
+        
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        
+        // Ejercicio 4:
+        
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 = 0) continue;
+            else System.out.println(i);
+                    
+        }
+        
+        // Ejercicio 5:
+        
+        for (int i = 1; i <=5; i++) {
+            
+            
+            
+            
+        }
+        
+        // Ejercicio 6:
+        
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(i);
+        }
+        
+        // Ejercicio 7:
+                
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
