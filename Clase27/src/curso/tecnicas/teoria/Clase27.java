bbcb9faa9c97735173c5091d0dae937c3c09e280
limonadademaracuya <126106463+limonadademaracuya@users.noreply.github.com>
2023-11-29 13:29:59 -0300

    colores inercalados

---
+++ Clase27/src/curso/tecnicas/teoria/Clase27.java
@@ -0,0 +1,26 @@
+
+package curso.tecnicas.teoria;
+
+public class Clase27 {
+
+    public static void main(String[] args) {
+        /*
+        AWT (Abstract Windows Toolkit) - SWING - JavaFX
+        
+        En Java existen muchas interfaces gráficas y se pueden agregar más de
+        forma externa.
+        La primera en aparecer fue AWT. Era un interfaz gráfica muy sencilla y 
+        veloz, estaba incluida dentro de la librería standard de Java. Pero el
+        mayor problema lo tenía en que no se veía igual en todos los SO.
+        Con el tiempo apareció SWING, que utiliza AWT pero garantiza que su aspecto
+        sea igual en todos los SO.
+        A partir del JDK 5 aparece JavaFX, que a partir de la versión JDK 7 comenzó
+        a formar parte integral del núcleo de Java. A partir de la versión JDK 10
+        ya no se incluye dentro del JDK standard de Oracle y pasó a ser parte de
+        una librería externa que debe descargarse para utilizarse. No tuvo mucho
+        éxito comercial.
+        Para este curso utilizaremos la Interfaz Gráfica SWING.
+        */
+    }
+    
+}
